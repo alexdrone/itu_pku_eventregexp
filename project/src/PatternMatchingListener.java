f9a2ac7ea2c8ef6adba76da75d45fd47beeff209
Alex Usbergo <alexakadrone@gmail.com>
2011-04-04 22:49:59 +0200

    alex no msg

--- project/src/PatternMatchingListener.java
+++ project/src/PatternMatchingListener.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.list.*;
 import dk.itu.infobus.ws.*;
 
 public class PatternMatchingListener extends Listener {
@@ -10,9 +10,9 @@ public class PatternMatchingListener extends Listener {
 	 * Creates a new <code>PatternMatchingListener</code> from a given stream
 	 * @param pattern The pattern that describes the stream 
 	 */
-	public PatternMatchingListener(List pattern) {
+	public PatternMatchingListener(PatternBuilder pattern) {
 		
-		listener = new Listener(pattern) {
+		listener = new Listener(pattern.getPattern()) {
 			
 			/* is used when the listener has to release some resource when 
 			 * it is removed from the server */
@@ -40,7 +40,7 @@ public class PatternMatchingListener extends Listener {
 	 * the identity of the listener, or that needs the listener to be 
 	 * already connected to the server.
 	 */
-	public void cleanUp() throws Exception { /* todo */ }
+	public void cleanUp() throws Exception() { /* todo */ }
 	
 	/** 
 	 * Called whenever an event is received for this listener
@@ -66,5 +66,23 @@ public class PatternMatchingListener extends Listener {
 	/* test main */
 	public static void main(String[] args) {
 		
+		EventBus eb = new EventBus("tiger.itu.dk",8004);
+		
+		eb.start();
+		eb.addGenerator(new SampleGenerator());
+		
+		/* new pattern */
+		PatternBuilder pb = new PatternBuilder()
+			.addMatchAll("foo")
+			.add("bar", PatternOperator.NEQ, 10);
+			
+		Listener listener = new PatternMatchingListener(pb) {
+			
+			/* user will mainly override this method */
+			public void onMessage(Map<String, Object> msg) {
+			    Util.traceEvent(msg);	
+			}
+
+		};
 	}
 }
