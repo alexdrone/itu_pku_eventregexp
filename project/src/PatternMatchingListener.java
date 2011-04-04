118440b186508f2ddcf5a889441db135b0960023
Alex Usbergo <alexakadrone@gmail.com>
2011-04-04 22:38:24 +0200

    alex no msg

---
+++ project/src/PatternMatchingListener.java
@@ -0,0 +1,66 @@
+import java.util.list.*;
+import dk.itu.infobus.ws.*;
+
+public class PatternMatchingListener extends Listener {
+	
+	/* the class stream */
+	private Listener listener;
+	
+	/**
+	 * Creates a new <code>PatternMatchingListener</code> from a given
+	 * stream
+	 * @param pattern The pattern that describes the stream 
+	 */
+	public PatternMatchingListener(List<ListenerToken<E>> pattern) {
+		
+		listener = new Listener(pattern) {
+			
+			/* is used when the listener has to release some resource when 
+			 * it is removed from the server */
+			public void cleanUp() throws Exception { /* todo */ }
+			
+			/* is called when an event matching the pattern has been 
+			 * received */
+			public void onMessage(Map<String, Object> msg) {
+				someLogic();
+				onMessage(msg);
+			}
+			
+			/* is used to define some initialization. It is called when the 
+			 * listener has been registered on the Genie Hub server */
+			public void onStarted() { /* todo */ }
+			
+		};
+		
+		/* todo: other initialization */
+	}
+	
+	/**
+	 * Called when the client receive the server-assigned registration 
+	 * identifier. Useful for one-time initializations that needs to know 
+	 * the identity of the listener, or that needs the listener to be 
+	 * already connected to the server.
+	 */
+	public void cleanUp() throws Exception() { /* todo */ }
+	
+	/** 
+	 * Called whenever an event is received for this listener
+	 */
+	@Override
+	public void onMessage(Map<String, Object> msg) { 
+		/* end-user will override this method */
+	}
+	
+	/**
+	 * Called when the listener has to be removed. Useful for freeing 
+	 * resources.
+	 */
+	public void onStarted() { /* todo */ }
+	
+	
+	public void someLogic() { 
+	
+		System.out.println("filtering the data.");
+		/* pattern matching logic */ 
+	}
+}
